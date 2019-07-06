6646e5bdfeaf97bed09f23489729e11270fc1ebd
Timo Streckfuss <timo.streckfuss@tado.com>
2019-07-06 21:39:14 +0200

    add card reader
    The card reader listens to events from an RFID card reader. The RFID
    card reader is found from the list of input devices with a fixed
    string.
    
    If an ENTER key is detected the reader has finished reading an RFID
    card. In this case the read card ID is logged and the reader is reset
    to start listening for the next card.

---
+++ eszett/reader/reader.py
@@ -0,0 +1,27 @@
+import logging
+from evdev import InputDevice, list_devices, ecodes
+
+
+class CardReader(object):
+    UP = 0
+    DEVICE_NAME = "HXGCoLtd Keyboard"
+
+    def __init__(self):
+        self.__logger = logging.getLogger(__name__)
+        devices = [InputDevice(path) for path in list_devices()]
+        self.__device = next(device for device in devices if device.name == self.DEVICE_NAME)
+        self.__card_id = ""
+
+    async def read_cards(self):
+        with self.__device.grab_context():
+            self.__logger.info("Starting to read cards")
+
+            async for event in self.__device.async_read_loop():
+                if event.type == ecodes.EV_KEY and event.value == self.UP:
+                    if event.code == ecodes.KEY_ENTER:
+                        self.__logger.info(f"Read Card (ID: {self.__card_id})")
+                        self.__card_id = ""
+                    else:
+                        self.__card_id += ecodes.KEY[event.code][-1:]
+
+
