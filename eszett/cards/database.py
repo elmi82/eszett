b66982ae26b90af3f403c1748abab0f55011d090
Timo Streckfuss <timo.streckfuss@tado.com>
2019-07-07 13:11:15 +0200

    initialize simple document database
    The document database will store the relation between card ID and
    playback details, such as songs to play and metadata.

--- eszett/cards/database.py
+++ eszett/cards/database.py
@@ -1,11 +1,18 @@
 import logging
+import os
 
 from asyncio import Queue
+from tinydb import TinyDB
+
+from eszett import resources
 
 
 class Database(object):
+    DB_PATH = f"{os.path.dirname(resources.__file__)}/database.json"
+
     def __init__(self, card_queue: Queue):
         self.__logger = logging.getLogger(__name__)
+        self.__db = TinyDB(Database.DB_PATH, sort_keys=True, indent=4, separators=(',', ': '))
         self.card_queue = card_queue
 
     async def listen(self):
