2f41a1673025acf47f9dee2d6f682398be4ecd0e
Timo Streckfuss <timo.streckfuss@tado.com>
2019-07-07 12:51:40 +0200

    write card ID to card queue
    Once a card is read by the reader the ID is put to the card queue. The
    part of the application that is handling playing music can pick up the
    read ID, lookup the song or playlist that is related to this ID and
    can start playing.

--- eszett/eszett.py
+++ eszett/eszett.py
@@ -7,7 +7,9 @@ from eszett.reader.reader import CardReader
 async def start():
     logging.info("eszett - easy-peasy casting")
 
-    reader = CardReader()
+    card_queue = asyncio.Queue()
+    reader = CardReader(card_queue)
 
     card_reader = asyncio.create_task(reader.read_cards())
     await asyncio.gather(card_reader)
+    await card_queue.join()
--- eszett/reader/reader.py
+++ eszett/reader/reader.py
@@ -1,4 +1,6 @@
 import logging
+
+from asyncio import Queue
 from evdev import InputDevice, list_devices, ecodes
 
 
@@ -6,8 +8,9 @@ class CardReader(object):
     UP = 0
     DEVICE_NAME = "HXGCoLtd Keyboard"
 
-    def __init__(self):
+    def __init__(self, card_queue: Queue):
         self.__logger = logging.getLogger(__name__)
+        self.card_queue = card_queue
         devices = [InputDevice(path) for path in list_devices()]
         self.__device = next(device for device in devices if device.name == self.DEVICE_NAME)
         self.__card_id = ""
@@ -20,6 +23,7 @@ class CardReader(object):
                 if event.type == ecodes.EV_KEY and event.value == self.UP:
                     if event.code == ecodes.KEY_ENTER:
                         self.__logger.info(f"Read Card (ID: {self.__card_id})")
+                        await self.card_queue.put(self.__card_id)
                         self.__card_id = ""
                     else:
                         self.__card_id += ecodes.KEY[event.code][-1:]
