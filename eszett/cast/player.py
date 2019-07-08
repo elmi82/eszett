c9eae405f0ffc3e091c942ad3b75994717ad5410
Timo Streckfuss <timo.streckfuss@tado.com>
2019-07-08 23:40:25 +0200

    connect to chromecast device
    The device to connect to is currently statically defined.

--- eszett/cast/player.py
+++ eszett/cast/player.py
@@ -1,11 +1,14 @@
 import logging
 import sys
 
+import pychromecast
 from asyncio import Queue
 from gmusicapi import Mobileclient
 
 
 class Player(object):
+    CHROMECAST_DEVICE_NAME = "Büro Mini"
+
     def __init__(self, playlist_queue: Queue):
         self.__logger = logging.getLogger(__name__)
         self.playlist_queue = playlist_queue
@@ -16,6 +19,11 @@ class Player(object):
         if not success:
             self.__logger.error("Login to google music failed")
             sys.exit(1)
+
+        chromecasts = pychromecast.get_chromecasts()
+        device = next(cc for cc in chromecasts if cc.device.friendly_name == Player.CHROMECAST_DEVICE_NAME)
+        device.wait()
+        self.__media_controller = device.media_controller
 
     async def handle_playlist_events(self):
         while True:
